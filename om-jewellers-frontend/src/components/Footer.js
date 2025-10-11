bc4d24cd5525c2e2155a37f9bd1d2920c8c43930
jwelleryshowroom <jwelleryshowroom@gmail.com>
2025-10-11 16:48:16 +0530

    deleted om

--- om-jewellers-frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 py-4 text-center text-sm text-gray-600">
-      © {new Date().getFullYear()} Jewellery Inventory. All rights reserved.
-    </footer>
-  );
-}
